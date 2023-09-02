0e39ef6233becbc2173a3918dc173ada5b0c2fef
Ola Senatov <ola@senatov.pro>
2023-09-02 17:56:38 +0300

    models created, dynamic html, django templates

--- art_market_project/artmarket/artmarket/settings.py
+++ art_market_project/artmarket/artmarket/settings.py
@@ -125,5 +125,8 @@ STATICFILES_DIRS = [
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
