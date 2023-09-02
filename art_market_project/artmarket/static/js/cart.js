048877e2d54f02d4b0ec0a1287e29f96c3e8f51e
Ola Senatov <ola@senatov.pro>
2023-09-03 00:20:07 +0300

    added logic to the cart, implemented js

---
+++ art_market_project/artmarket/static/js/cart.js
@@ -0,0 +1,41 @@
+console.log('Hello World, this is cart')
+
+let updateBtns = document.getElementsByClassName('update-cart')
+
+for (i=0; i< updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log('productId: ', productId, 'action: ', action )
+
+        console.log('USER: ', user)
+        if (user == 'AnonymousUser'){
+            console.log('User is not authenticated')
+        }else{
+            updateUserOrder(productId, action)
+        }
+    })
+}
+
+function updateUserOrder(productId, action) {
+    console.log('User is authenticated, sending data...')
+
+        let url = '/update_item/'
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrftoken,
+            },
+            body:JSON.stringify({'productId': productId, 'action' : action}) 
+        })
+
+        .then((response ) => {
+            return response.json()
+        })
+
+        .then((data ) => {
+            return response.json('data', data)
+         })
+}
--- art_market_project/artmarket/store/views.py
+++ art_market_project/artmarket/store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+import datetime
 
 # Create your views here.
 
@@ -37,3 +41,57 @@ def checkout(request):
     context = {'items': items,
                'order': order }
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
+@csrf_exempt       
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+			customer=customer,
+			order=order,
+			address=data['shipping']['address'],
+			city=data['shipping']['city'],
+			state=data['shipping']['state'],
+			zipcode=data['shipping']['zipcode'],
+			)
+	else:
+		print('User is not logged in')
+
+	return JsonResponse('Payment submitted..', safe=False)
